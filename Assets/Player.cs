0c61f5ebcc98bdc70cf11012a0861dc2cf8c663d
fihra <fabrofabian@gmail.com>
2022-01-24 16:40:17 -1000

    created quick coin collecting game with player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 100.0f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 tempVect = new Vector3(horizontal, vertical, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
+
+    }
+}
